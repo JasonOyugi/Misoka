03b73379b53ea440aaabbcfcd3794e53f08b0230
JasonOyugi <Jason.oyugi@gatsbyafrica.org.uk>
2025-09-15 09:30:59 +0300

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -30,19 +30,18 @@ const About = () => {
     <div id="about" className="min-h-screen w-screen">
       <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
         <p className="font-general text-sm uppercase md:text-[10px]">
-          Welcome to Zentry
+          Welcome to Misoka
         </p>
 
         <AnimatedTitle
-          title="Disc<b>o</b>ver the world's <br /> largest shared <b>a</b>dventure"
+          title="Ch<b>ang</b>e your world <br /> own the <b>future</b>"
           containerClass="mt-5 !text-black text-center"
         />
 
         <div className="about-subtext">
-          <p>The Game of Games begins—your life, now an epic MMORPG</p>
           <p className="text-gray-500">
-            Zentry unites every player from countless games and platforms, both
-            digital and physical, into a unified Play Economy
+          The solution to high-impact, high-return investments in Africa is here:
+          We are specialists in turning underutilized/degraded land into high-return assets.
           </p>
         </div>
       </div>
